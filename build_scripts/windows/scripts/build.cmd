5a4796f2e079aab4c72d60de457a5b7f312faebb
Troy Dai <troy.dai@outlook.com>
2018-10-03 03:23:24 -0700

    [skip ci] Update msi build script

--- build_scripts/windows/scripts/build.cmd
+++ build_scripts/windows/scripts/build.cmd
@@ -196,7 +196,7 @@ for /d /r %BUILDING_DIR%\Lib\site-packages\pip %%d in (__pycache__) do (
 if %errorlevel% neq 0 goto ERROR
 
 echo Building MSI...
-msbuild /t:rebuild /p:Configuration=Release %REPO_ROOT%\build_scripts\windows\azure-cli.wixproj
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" /t:rebuild /p:Configuration=Release %REPO_ROOT%\build_scripts\windows\azure-cli.wixproj
 
 start %OUTPUT_DIR%
 
